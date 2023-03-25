0b05673f6e9a31e590851075770cc16927e266a0
rkotkiewicz <you@example.com>
2023-03-25 21:53:28 +0100

    refactor tests

---
+++ plugin/src/functionalTest/kotlin/com/github/rkotkiewicz/FunctionalTestBase.kt
@@ -0,0 +1,47 @@
+package com.github.rkotkiewicz
+
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.io.TempDir
+import java.io.File
+
+open class FunctionalTestBase {
+
+    @field:TempDir
+    lateinit var projectDir: File
+
+    protected val buildFile by lazy { projectDir.resolve("build.gradle") }
+    private val settingsFile by lazy { projectDir.resolve("settings.gradle") }
+
+    private val srcFolder by lazy { projectDir.resolve("src") }
+    private val srcSubFolder by lazy { srcFolder.resolve("subFolder") }
+
+    protected val templateFile0 by lazy { srcFolder.resolve("test0.template.txt") }
+    protected val templateFile1 by lazy { srcSubFolder.resolve("test1.template.txt") }
+
+    protected val buildDir by lazy { projectDir.resolve("build") }
+
+    @BeforeEach
+    fun initProject() {
+        // Set up the test build
+        settingsFile.writeText("")
+        buildFile.writeText(
+            """
+            plugins {
+                id('com.github.rkotkiewicz.template')
+            }
+            
+            """.trimIndent()
+        )
+        srcSubFolder.mkdirs()
+    }
+    protected fun gradlew(vararg args: String): BuildResult {
+        val runner = GradleRunner.create()
+        runner.forwardOutput()
+        runner.withPluginClasspath()
+        runner.withArguments(args.toList())
+        runner.withProjectDir(projectDir)
+        return runner.build()
+    }
+}
