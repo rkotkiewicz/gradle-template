33d40780979045b486b7010893df7946578f169d
rkotkiewicz <you@example.com>
2023-03-25 21:53:28 +0100

    refactor tests

---
+++ plugin/src/functionalTest/kotlin/com/github/rkotkiewicz/TemplatePluginBindingFunctionalTest.kt
@@ -0,0 +1,124 @@
+package com.github.rkotkiewicz
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class TemplatePluginBindingFunctionalTest: FunctionalTestBase() {
+
+
+    @Test
+    fun `a template given by a file uses binding`() {
+
+        buildFile.appendText(
+            """                
+            template {
+                create('test') {
+                    from.set(file("${'$'}projectDir/src/${templateFile0.name}"))
+                    binding(stringValue : "sssstring",
+                        intValue : 42 ,
+                        stringList : ["no0", "no1", "no10"]
+                    )
+                }
+            }           
+            
+        """.trimIndent()
+        )
+        templateFile0.writeText(
+            """
+            String value: ${'$'}stringValue
+            Int value: ${'$'}intValue
+            Strings list:<% for (str in stringList) { %>
+              String: <%= str %><% } %>
+        """.trimIndent()
+        )
+
+        // Run the build
+        gradlew(":testFillTemplate")
+
+        // Verify the result
+        assertEquals(
+            """
+            String value: sssstring
+            Int value: 42
+            Strings list:
+              String: no0
+              String: no1
+              String: no10
+        """.trimIndent(), buildDir.resolve("template/test/test0.template.txt").readText()
+        )
+    }
+
+
+    @Test
+    fun `a template supports ext properties binding`() {
+
+        buildFile.appendText(
+            """                
+            ext {
+                str = "it is a string"
+            }
+                
+            template {
+                create('test') {
+                    from.set(file("${'$'}projectDir/src/${templateFile0.name}"))
+                    binding(stringValue : str)
+                }
+            }           
+            
+        """.trimIndent()
+        )
+        templateFile0.writeText("value: \$stringValue")
+
+        // Run the build
+        gradlew(":testFillTemplate")
+
+        // Verify the result
+        assertEquals("value: it is a string",
+            buildDir.resolve("template/test/test0.template.txt").readText()
+        )
+    }
+
+
+    @Test
+    fun `templates given by a directory use binding`() {
+
+        buildFile.appendText(
+            """                
+            template {
+                create('testTwo') {
+                    from.set(file("${'$'}projectDir/src/"))
+                    binding(question : "What is the reason?",
+                        answer : 101010
+                    )
+                }
+            }           
+            
+        """.trimIndent()
+        )
+        templateFile0.writeText(
+            """
+            The question: "${'$'}question"
+        """.trimIndent()
+        )
+        templateFile1.writeText(
+            """
+            The answer: "${'$'}answer"
+        """.trimIndent()
+        )
+
+        // Run the build
+        gradlew(":testFillTemplate")
+
+        // Verify the result
+        assertEquals(
+            """
+            The question: "What is the reason?"
+        """.trimIndent(), buildDir.resolve("template/testTwo/test0.template.txt").readText()
+        )
+        assertEquals(
+            """
+            The answer: "101010"
+        """.trimIndent(), buildDir.resolve("template/testTwo/subFolder/test1.template.txt").readText()
+        )
+    }
+}
--- plugin/src/functionalTest/kotlin/com/github/rkotkiewicz/TemplatePluginFunctionalTest.kt
+++ plugin/src/functionalTest/kotlin/com/github/rkotkiewicz/TemplatePluginFunctionalTest.kt
@@ -1,46 +1,12 @@
 package com.github.rkotkiewicz
 
-import org.gradle.testkit.runner.GradleRunner
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.io.TempDir
-import java.io.File
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-class TemplatePluginFunctionalTest {
-
-    @field:TempDir
-    lateinit var projectDir: File
-
-    private val buildFile by lazy { projectDir.resolve("build.gradle") }
-    private val settingsFile by lazy { projectDir.resolve("settings.gradle") }
-
-    private val srcFolder by lazy { projectDir.resolve("src") }
-    private val srcSubFolder by lazy { srcFolder.resolve("subFolder") }
-
-    private val templateFile0 by lazy { srcFolder.resolve("test0.template.txt") }
-    private val templateFile1 by lazy { srcSubFolder.resolve("test1.template.txt") }
-
-    private val buildDir by lazy { projectDir.resolve("build") }
-
-    @BeforeEach
-    fun initProject() {
-        // Set up the test build
-        settingsFile.writeText("")
-        buildFile.writeText(
-            """
-            plugins {
-                id('com.github.rkotkiewicz.template')
-            }
-            
-            """.trimIndent()
-        )
-        srcSubFolder.mkdirs()
-    }
-
+class TemplatePluginFunctionalTest: FunctionalTestBase()  {
 
     @Test
-    fun `generated task 'testFileFillTemplate' produces output file for input file`() {
+    fun `a generated task 'testFileFillTemplate' produces an output file for an input file`() {
 
         buildFile.appendText(
             """                
@@ -55,15 +21,16 @@ class TemplatePluginFunctionalTest {
         templateFile0.writeText("whatever")
 
         // Run the build
-        runTask(":testFileFillTemplate")
+        gradlew(":testFileFillTemplate")
 
         // Verify the result
         assertEquals("whatever", buildDir.resolve("template/testFile/test0.template.txt").readText())
     }
 
 
+
     @Test
-    fun `generated task 'testDirFillTemplate' produces output files for input directory`() {
+    fun `a generated task 'testDirFillTemplate' produces output files for an input directory`() {
         buildFile.appendText(
             """            
             template {
@@ -77,7 +44,7 @@ class TemplatePluginFunctionalTest {
         templateFile1.writeText("second file")
 
         // Run the build
-        runTask(":testDirFillTemplate")
+        gradlew(":testDirFillTemplate")
 
         // Verify the result
         val destinationDir = buildDir.resolve("template/testDir")
@@ -85,100 +52,4 @@ class TemplatePluginFunctionalTest {
         assertEquals("second file", destinationDir.resolve("subFolder/test1.template.txt").readText())
     }
 
-
-    @Test
-    fun `generated task 'testFillTemplate' fill template into output file`() {
-
-        buildFile.appendText(
-            """                
-            template {
-                create('test') {
-                    from.set(file("${'$'}projectDir/src/${templateFile0.name}"))
-                    binding(stringValue : "sssstring",
-                        intValue : 42 ,
-                        stringList : ["no0", "no1", "no10"]
-                    )
-                }
-            }           
-            
-        """.trimIndent()
-        )
-        templateFile0.writeText(
-            """
-            String value: ${'$'}stringValue
-            Int value: ${'$'}intValue
-            Strings list:<% for (str in stringList) { %>
-              String: <%= str %><% } %>
-        """.trimIndent()
-        )
-
-        // Run the build
-        runTask(":testFillTemplate")
-
-        // Verify the result
-        assertEquals(
-            """
-            String value: sssstring
-            Int value: 42
-            Strings list:
-              String: no0
-              String: no1
-              String: no10
-        """.trimIndent(), buildDir.resolve("template/test/test0.template.txt").readText()
-        )
-    }
-
-    @Test
-    fun `generated task 'testTwoFillTemplate' fill templates into output files`() {
-
-        buildFile.appendText(
-            """                
-            template {
-                create('testTwo') {
-                    from.set(file("${'$'}projectDir/src/"))
-                    binding(question : "What is the reason?",
-                        answer : 101010
-                    )
-                }
-            }           
-            
-        """.trimIndent()
-        )
-        templateFile0.writeText(
-            """
-            The question: "${'$'}question"
-        """.trimIndent()
-        )
-        templateFile1.writeText(
-            """
-            The answer: "${'$'}answer"
-        """.trimIndent()
-        )
-
-        // Run the build
-        runTask(":testFillTemplate")
-
-        // Verify the result
-        assertEquals(
-            """
-            The question: "What is the reason?"
-        """.trimIndent(), buildDir.resolve("template/testTwo/test0.template.txt").readText()
-        )
-        assertEquals(
-            """
-            The answer: "101010"
-        """.trimIndent(), buildDir.resolve("template/testTwo/subFolder/test1.template.txt").readText()
-        )
-    }
-
-    private fun runTask(taskName: String) {
-        val runner = GradleRunner.create()
-        runner.forwardOutput()
-        runner.withPluginClasspath()
-        runner.withArguments(taskName)
-        runner.withProjectDir(projectDir)
-        runner.withDebug(true)
-        runner.build()
-
-    }
 }
